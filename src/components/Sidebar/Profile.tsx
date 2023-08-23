380fe93c1dd7e65d446adb60bad080a0274d366d
killer-cf <costa.kilder@live.com>
2023-08-23 15:37:12 -0300

    style Profile and UserSpace

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,29 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/killer-cf.png"
+        alt="user image"
+        className="h-10 w-10 rounded-full"
+      />
+
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Kilder costa
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          costa.kilder@live.com
+        </span>
+      </div>
+
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-500"
+      >
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
